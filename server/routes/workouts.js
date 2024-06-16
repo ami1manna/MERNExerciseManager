d1cfeda8fd57ce92c4b5b7c1ebdb66cf7ec1b7d6
ami1manna <amitmannasm@gmail.com>
2024-06-16 15:55:12 +0530

    added dummy server

---
+++ server/routes/workouts.js
@@ -0,0 +1,31 @@
+const express =  require('express');
+
+const router = express.Router();
+
+//GET all workouts
+router.get('/',(req,res)=>{
+    res.json({mssg:"GETall wrokouts"})
+})
+
+//GET a single  workout
+router.get('/:id',(req,res)=>{
+    res.json({mssg:'GET a single Workout'})
+})
+
+//POST a new workout
+router.post('/',(req,res)=>{
+    res.json({mssg:'POST a new Workout'})
+})
+
+//DELETE a  workout
+router.delete('/:id',(req,res)=>{
+    res.json({mssg:'DELETE a  Workout'})
+})
+
+//UPDATE a new workout
+router.patch('/:id',(req,res)=>{
+    res.json({mssg:'UPDATE a new Workout'})
+})
+
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 
+//import routes 
+const workoutRoutes = require('./routes/workouts')
+
 //express app 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.json({mssg:'Welcome user'})
-}
-)
+//middle ware
+app.use(express.json());// added for putting data to be send to server
+app.use((req,res,next)=>
+    {
+        console.log(req.path,req.method)
+        next();
+    })
+
+//routes
+app.use('/api/workouts',workoutRoutes)
+
+
 //listen or request
 app.listen(process.env.PORT,()=>{
     console.log('listening on port 4000 !!')
